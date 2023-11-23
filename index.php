0d185c9e72b5e65642f5e0f09eac77d113c9dd76
eka yuniar <sayayuniar@gmail.com>
2023-11-23 19:33:46 +0700

    Perintah Tugas 5

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$nama = "Yuniar";
+$x = 10;
+$y = 20;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,15 +8,24 @@ $nama = "Yuniar";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar PHP</title>
+    <title>Hasil Aritmatika</title>
 </head>
 
 <body>
-    <h1> Selamat Datang
-        <?php
-        echo $nama;
-        ?>
-    </h1>
+    <h2>Hasil dari Penjumlahan <?= $x + $y;  ?>
+        <br>
+    </h2>
+    <h2>Hasil dari Pengurangan <?= $x - $y;  ?>
+        <br>
+    </h2>
+    <h2>Hasil dari Perkalian <?= $x * $y;  ?>
+        <br>
+    </h2>
+    <h2>Hasil dari Pembagian <?= $x / $y;  ?>
+        <br>
+    </h2>
+    <h2>Hasil dari Modus <?= $x % $y;  ?>
+    </h2>
 </body>
 
 </html>
