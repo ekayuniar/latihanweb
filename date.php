46d14ddf92f0fecf836f3a50f31169fb9e477e47
eka yuniar <sayayuniar@gmail.com>
2023-11-30 19:52:19 +0700

    task 2

--- date.php
+++ date.php
@@ -1,6 +1,6 @@
 <?php
 $todayDate = "30 nov 2023"; //pendefinisian tanggal awal  
-$returnDate = date('d-m-Y', strtotime('+100 day', strtotime($todayDate))); //operasi penjumlahan tanggal sebanyak 7 hari 
+$returnDate = date('d-m-Y', strtotime('+7 Year', strtotime($todayDate))); //operasi penjumlahan tanggal sebanyak 7 hari 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +15,7 @@ $returnDate = date('d-m-Y', strtotime('+100 day', strtotime($todayDate))); //ope
     <h3>
         Tanggal hari ini = <?= $todayDate;  ?>
         <br>
-        Tanggal 100 hari dari skrg = <?= $returnDate;  ?>
+        Tanggal 7 hari dari skrg = <?= $returnDate;  ?>
     </h3>
 </body>
 
